7f9c8a0baa3a0d7607cf8ebfcdcaad90fb3c28a8
msi1427 <sabikirbaz@iut-dhaka.edu>
2020-01-03 11:18:09 +0600

    Server cannot cache, enhanced security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_cache_buster
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
   def configure_permitted_parameters
     devise_parameter_sanitizer
         .permit(:sign_up) do |u| u.permit(:name, :std_id, :department, :semester, :role, :email, :password )
